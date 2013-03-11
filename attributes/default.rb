29299df512c03f520c105c3ce2f4c12357eabeca
Scott Klein <scott@scottkle.in>
2013-03-11 12:37:16 -0600

    thundercats a ready with ruby 2

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,2 +1,2 @@
-default['ruby']['version'] = '1.9.3-p327'
-default['ruby']['checksum'] = '51dd76462d3f6eb2c659a75e90f949f56da58c42bfb5766212478160b7f23d71'
+default['ruby']['version'] = '2.0.0-p0'
+default['ruby']['checksum'] = 'aff85ba5ceb70303cb7fb616f5db8b95ec47a8820116198d1c866cc4fff151ed'
