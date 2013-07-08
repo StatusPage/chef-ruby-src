7ba910ffb7b98688a1fa46f7555ded7c08d58bec
Eric Jensen <ericcj@gmail.com>
2013-07-08 09:54:18 -0400

    remove custom chef stuff since modern gem works with ruby 2.0 and bump default 2.0 version

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,6 +1,6 @@
 
-default['ruby']['version'] = '2.0.0-p0'
-default['ruby']['checksum'] = 'aff85ba5ceb70303cb7fb616f5db8b95ec47a8820116198d1c866cc4fff151ed'
+default['ruby']['version'] = '2.0.0-p247'
+default['ruby']['checksum'] = '3e71042872c77726409460e8647a2f304083a15ae0defe90d8000a69917e20d3'
 
 default['ruby']['prefix']         = "/usr/local"
 default['ruby']['configure_opts'] = " --prefix='#{node[:ruby][:prefix]}' "
